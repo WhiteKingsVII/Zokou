e5d51abefefaa6db18183a721bd14c9f80b0fb5a
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-01-08 00:12:03 +0000

    Update north1fiche.js

--- bdd/north1fiche.js
+++ bdd/north1fiche.js
@@ -88,10 +88,11 @@ async function getData() {
   const client = await pool.connect();
 
   try {
-    const query = 'SELECT * FROM north4_he';
+    const query = 'SELECT r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21, r22, r23, r24, r25, r26, r27, r28, r29, r30, r31, r32, r33, r34, r35, r36 FROM north4_he';
     const result = await client.query(query);
-
-    return result.rows;
+    const { r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21, r22, r23, r24, r25, r26, r27, r28, r29, r30, r31, r32, r33, r34, r35, r36 } = result.row[0];  
+     
+    return { r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21, r22, r23, r24, r25, r26, r27, r28, r29, r30, r31, r32, r33, r34, r35, r36 }
   } catch (error) {
     console.error('Erreur lors de la récupération des données:', error);
   } finally {
