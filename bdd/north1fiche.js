90a510d02460f86d337865d1100c3d0a0f4a8bef
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-01-09 16:53:47 +0000

    Update north1fiche.js

--- bdd/north1fiche.js
+++ bdd/north1fiche.js
@@ -67,25 +67,28 @@ async function createNorth1FicheTable() {
 // Fonction pour insérer des données
 async function insertData() {
   const client = await pool.connect();
-const aquery = 'SELECT r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21, r22, r23, r24, r25, r26, r27, r28, r29, r30, r31, r32, r33, r34, r35, r36 FROM north4_ WHERE id =1';
+
+  try {
+    const aquery = 'SELECT r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21, r22, r23, r24, r25, r26, r27, r28, r29, r30, r31, r32, r33, r34, r35, r36 FROM north4_ WHERE id = 1';
     const result = await client.query(aquery);
 
-    return result.rows;
+    if (result.rows.length === 0) {
+      const query = `
+        INSERT INTO north4_ (id, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21, r22, r23, r24, r25, r26, r27, r28, r29, r30, r31, r32, r33, r34, r35, r36)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31, $32, $33, $34, $35, $36)
+      `;
 
- if (result.rows.length === 0) try {
-    const query = `
-      INSERT INTO north4_ (id, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21, r22, r23, r24, r25, r26, r27, r28, r29, r30, r31, r32, r33, r34, r35, r36)
-      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31, $32, $33, $34, $35, $36)` ,[1, 'aucun', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'aucun', 'aucun', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'aucun', 'aucun', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'aucun']);
-
-    await client.query(query);
-    console.log('Données insérées avec succès');
+      await client.query(query, [1, 'aucun', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'aucun', 'aucun', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'aucun', 'aucun', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'aucun']);
+      console.log('Données insérées avec succès');
+    } else {
+      console.log('Les données existent déjà. Aucune insertion nécessaire.');
+    }
   } catch (error) {
     console.error('Erreur lors de l\'insertion des données:', error);
   } finally {
     client.release();
   }
 }
-
 // Fonction pour récupérer toutes les données
 async function getData() {
   const client = await pool.connect();
